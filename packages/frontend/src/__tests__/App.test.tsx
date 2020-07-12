9b3e9eba14ee1453b8f260951e607ea87c4f7797
Michał Stocki <michal.stocki@uxpin.com>
2020-07-12 02:40:07 +0200

    Remove duplicated test
    (header already tested in e2e)

--- packages/frontend/src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import { App } from '../App';
-
-it('renders App header', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Deployments/i);
-  expect(linkElement).toBeInTheDocument();
-});
